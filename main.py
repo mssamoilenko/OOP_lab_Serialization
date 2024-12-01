4e0ed6920df561b88428b6dad2fe416def308668
Alla <allasamoilenko@gmx.de>
2024-12-01 22:33:48 +0200

    task3,4,5

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pickle
+import time
+
 #task1
 class UsersDict:
     def __init__(self):
@@ -95,3 +97,99 @@ serialized_session = pickle.dumps(session)
 loaded_session = pickle.loads(serialized_session)
 print("After deserialization:", loaded_session)
 
+#task3
+class DatabaseConnection:
+    def __init__(self, db_url, connection=None):
+        self.db_url = db_url
+        self.connection = connection
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state['connection'] = False
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.connection = True
+
+db = DatabaseConnection("example_db_url")
+print("До десеріалізації:", db.connection)
+serialized_db = pickle.dumps(db)
+restored_db = pickle.loads(serialized_db)
+print("Після десеріалізації:", restored_db.connection)
+
+#task4
+class Task:
+    def __init__(self, name, completed=False):
+        self.name = name
+        self.completed = completed
+
+    def __repr__(self):
+        return f"Task(name='{self.name}', completed={self.completed})"
+
+class TaskQueue:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def complete_task(self, task_name):
+        for task in self.tasks:
+            if task.name == task_name:
+                task.completed = True
+                break
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state['tasks'] = [task for task in self.tasks if not task.completed]
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+queue = TaskQueue()
+queue.add_task(Task("Вигуляти кота"))
+queue.add_task(Task("Помити посуд", completed=True))
+queue.add_task(Task("Зробити домашку"))
+print("Перед серіалізацією:", queue.tasks)
+serialized_queue = pickle.dumps(queue)
+restored_queue = pickle.loads(serialized_queue)
+print("Після десеріалізації:", restored_queue.tasks)
+
+#task5
+class Cache:
+    def __init__(self):
+        self.data = {}
+        self.expiration_time = 5
+    def set(self, key, value):
+        self.data[key] = (value, time.time())
+
+    def get(self, key):
+        if key in self.data:
+            value, timestamp = self.data[key]
+            if time.time() - timestamp < self.expiration_time:
+                return value
+            else:
+                del self.data[key]
+        return None
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state['data'] = {key: value for key, value in self.data.items() if time.time() - value[1] < self.expiration_time}
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+cache = Cache()
+cache.set("key1", "value1")
+cache.set("key2", "value2")
+
+print("Перед серіалізацією:", cache.data)
+time.sleep(3)
+serialized_cache = pickle.dumps(cache)
+restored_cache = pickle.loads(serialized_cache)
+print("Після десеріалізації:", restored_cache.data)
+time.sleep(3)
+print("Актуальні дані після закінчення терміну дії:", restored_cache.data)
